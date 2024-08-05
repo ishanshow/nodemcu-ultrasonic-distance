36e9d5a57bba5ffe8c2b9eb6ec06e74d435f794a
Ishan Show <105852558+ishanshow@users.noreply.github.com>
2024-08-06 00:00:32 +0530

    Update ultrasonic-distance.ino

--- ultrasonic-distance.ino
+++ ultrasonic-distance.ino
@@ -1,9 +1,10 @@
 const int trigPin = 12;    // SR04 trigger pin
 const int echoPin = 14;    // SR04 echo pin
 const int motorPin = 5;    // vibration motor digital pin
-//duration
+
 //define sound velocity in cm/uS
 #define SOUND_VELOCITY 0.034
+//define cm to inch conversion
 #define CM_TO_INCH 0.393701
 
 long duration;
